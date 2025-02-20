8230fb9b2b021717810f6add2069d1b21dedeefd
EGAMAGZ <46827955+EGAMAGZ@users.noreply.github.com>
2025-02-20 08:49:36 -0600

    feat: updated dependencies and improve CLI initialization

--- src/mod.ts
+++ src/mod.ts
@@ -1,9 +1,11 @@
 import { parseArgs } from "@std/cli/parse-args";
 import { init } from "./init.ts";
+
 const flags = parseArgs(Deno.args, {
   boolean: ["verbose"],
   alias: {
     help: "h",
   },
 });
+
 init(Deno.cwd(), flags);
