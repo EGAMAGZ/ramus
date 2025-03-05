1944895dc2ba0c02f7351237b34705f73eda14d2
EGAMAGZ <46827955+EGAMAGZ@users.noreply.github.com>
2025-03-04 22:57:16 -0600

    chore: Installed @std/fmt and configure to not generate lock file

--- src/init.ts
+++ src/init.ts
@@ -1,4 +1,6 @@
 import * as path from "@std/path";
+import { simpleGit } from "simple-git";
+
 export function init(
   cwd = Deno.cwd(),
   input: (string | number)[],
@@ -13,11 +15,19 @@ export function init(
     unresolvedDirectory = userInput;
   }
   const repositoryDir = path.resolve(cwd, unresolvedDirectory);
+
   try {
     const dir = [...Deno.readDirSync(repositoryDir)];
-    console.log(repositoryDir);
 
     const gitDir = dir.find((d) => d.name === ".git");
+    if (!gitDir) {
+      console.error("Project doesn't have git initialized.");
+      throw new Error("Error");
+    }
+
+    const git = simpleGit(repositoryDir);
+
+    console.log(repositoryDir);
     console.log(gitDir);
   } catch (err) {
     if (!(err instanceof Deno.errors.NotFound)) {
