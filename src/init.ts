454da34074e5b2fc98f6b4392ce22698a873d318
EGAMAGZ <46827955+EGAMAGZ@users.noreply.github.com>
2025-03-04 22:04:31 -0600

    feat: update dependencies and enhance CLI initialization with user input for repository path

--- src/init.ts
+++ src/init.ts
@@ -1,8 +1,28 @@
+import * as path from "@std/path";
 export function init(
   cwd = Deno.cwd(),
-  flags: {
-    verbose: boolean;
-  },
+  input: (string | number)[],
 ) {
-  console.log(cwd);
+  let unresolvedDirectory = Deno.args[0];
+
+  if (input.length !== 1) {
+    const userInput = prompt("Repository path: ");
+    if (!userInput) {
+      throw new Error();
+    }
+    unresolvedDirectory = userInput;
+  }
+  const repositoryDir = path.resolve(cwd, unresolvedDirectory);
+  try {
+    const dir = [...Deno.readDirSync(repositoryDir)];
+    console.log(repositoryDir);
+
+    const gitDir = dir.find((d) => d.name === ".git");
+    console.log(gitDir);
+  } catch (err) {
+    if (!(err instanceof Deno.errors.NotFound)) {
+      console.log("Error");
+      throw err;
+    }
+  }
 }
--- src/mod.ts
+++ src/mod.ts
@@ -1,11 +1,13 @@
 import { parseArgs } from "@std/cli/parse-args";
 import { init } from "./init.ts";
-
 const flags = parseArgs(Deno.args, {
-  boolean: ["verbose"],
   alias: {
     help: "h",
   },
 });
 
-init(Deno.cwd(), flags);
+try {
+  init(Deno.cwd(), flags._);
+} catch (err) {
+  throw err;
+}
